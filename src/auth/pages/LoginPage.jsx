c5db0a5b00eebda47909d2fcf96c181feed283bc
Ariel Arzamendia <arzamendiaariel@gmail.com>
2022-12-28 16:21:45 -0300

    loggin

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,18 +1,24 @@
-import { useNavigate } from "react-router-dom"
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
   const onLogin = () => {
-    navigate("/", {
-      replace: true
+    login('Ariel Arzamendia');
+    navigate('/', {
+      replace: true,
     });
   };
   return (
     <div className="container mt-5">
       <h1>LoginPage</h1>
       <hr />
-      <button className="btn btn-primary"
-        onClick={onLogin}>
+      <button
+        className="btn btn-primary"
+        onClick={onLogin}
+      >
         Login
       </button>
     </div>
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,12 +1,16 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 export const Navbar = () => {
+  const { logged, user, logout } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
   const onLogout = () => {
-    navigate('/login', { replace: true});
-   }
+    logout();
+    navigate('/login', { replace: true });
+  };
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link
@@ -48,16 +52,28 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          {/* <NavLink
-            className={({ isActive }) =>
-              `nav-item nav-link ${isActive ? 'active' : ''}`
-            }
-            to="/login"
-          >
-            Logout
-          </NavLink> */}
-          <span className="nav-item nav-link text-primary">Ariel</span>
-          <button onClick={onLogout} className="nav-item nav-link btn">Logout</button>
+          {logged ? (
+            <>
+              <span className="nav-item nav-link text-primary">
+                {user?.name}
+              </span>
+              <button
+                onClick={onLogout}
+                className="nav-item nav-link btn"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <NavLink
+              className={({ isActive }) =>
+                `nav-item nav-link ${isActive ? 'active' : ''}`
+              }
+              to="/login"
+            >
+              Login
+            </NavLink>
+          )}
         </ul>
       </div>
     </nav>
